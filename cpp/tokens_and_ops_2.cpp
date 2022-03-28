8befeb90fbb131618e217381d19d6af483fcbf07
magnusjwatson2786 <62503589+magnusjwatson2786@users.noreply.github.com>
2022-03-28 15:35:09 +0530

    Create tokens_and_ops_2.cpp

---
+++ cpp/tokens_and_ops_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float f,g;
+    
+    
+    cout<< "Enter two numbers: ";
+    cin >> f >>g;
+    cout<< "Product = " << f*g<<endl;
+    
+
+    return 0;
+}
