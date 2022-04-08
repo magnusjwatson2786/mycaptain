233ae1bf84c7eaab78fe987143d1323a99ad26dd
magnusjwatson2786 <62503589+magnusjwatson2786@users.noreply.github.com>
2022-04-08 10:36:50 +0530

    Update data_structures_filename.py

--- python/data_structures_filename.py
+++ python/data_structures_filename.py
@@ -1,10 +1,5 @@
 y=input("Input the Filename : ")
-ft=""
-if y.endswith(".py"):
-	ft="python"
-elif y.endswith(".txt"):
-	ft="text"
-elif y.endswith(".pdf"):
-	ft="Document"
 
-print(f"The extension of the file is : '{ft}'")
+
+
+print(f"The extension of the file is : '{y.split(".")[-1]}'")
